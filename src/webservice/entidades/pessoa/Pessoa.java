30334dd6ae7029a5dde7f6e56661a4a7a1fb29bc
matheus-souza <mh.matheussouza@gmail.com>
2016-11-21 22:49:45 -0200

    Adicionando classes do pacote Pessoa

---
+++ src/webservice/entidades/pessoa/Pessoa.java
@@ -0,0 +1,13 @@
+package webservice.entidades.pessoa;
+
+/**
+ *
+ * @author matheush
+ */
+abstract class Pessoa {
+    protected String id;
+    protected String nome;
+    protected String email;
+    protected String codConfirmacao;
+    protected String senha; 
+}
